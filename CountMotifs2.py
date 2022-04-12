d494800429195582824a2dd6d323dc8682a51c99
Irene Escobar <irene.e06@gmail.com>
2022-04-12 19:10:06 -0300

    CountMotifs2

---
+++ CountMotifs2.py
@@ -0,0 +1,36 @@
+def Count(Motifs):
+
+    count = {}
+    k = len(Motifs[0])
+    for symbol in "ACGT":
+        count[symbol] = []
+        for j in range(k):
+            count[symbol].append(0)
+    t = len(Motifs)
+    for i in range(t):
+        for j in range(k):
+            symbol = Motifs[i][j]
+            count[symbol][j] += 1
+    return count
+def Consensus(Motifs):
+    k = len(Motifs[0])
+    count = Count(Motifs)
+
+    consensus = ""
+    for j in range(k):
+        m = 0
+        frequentSymbol = ""
+        for symbol in "ACGT":
+            if count[symbol][j] > m:
+                m = count[symbol][j]
+                frequentSymbol = symbol
+        consensus += frequentSymbol
+    return consensus
+def Score(Motifs):
+    consensus = Consensus(Motifs)
+    count = 0
+    for motif in Motifs:
+        for index in range(len(motif)):
+            if motif[index] != consensus[index]:
+                count += 1
+    return count
